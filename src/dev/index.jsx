30bb8a436f19a27f553190fe21a45dea80679cc6
Sergey Arustamyan <arustamyan@ro.ru>
2021-02-16 21:25:56 +0400

    dev serve

---
+++ src/dev/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import ReactCompassNavigator from '../ReactCompassNavigator.jsx'
+
+render(<ReactCompassNavigator />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,12 @@
 const webpack = require('webpack')
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/ReactCompassNavigator.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: {
-      type: "commonjs",
-      name: "ReactCompassNavigator",
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
+module.exports = (env, options) => {
+
+  const plugins = [
     new MiniCssExtractPlugin({
       filename: 'master.css'
     }),
@@ -26,26 +16,52 @@ module.exports = {
       },
       canPrint: true
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, "src")
-        ],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+  ]
+
+  let externals = ['react']
+
+  let entry = './src/ReactCompassNavigator.jsx'
+  if (options.env.WEBPACK_SERVE) {
+    entry = './src/dev/index.jsx'
+    plugins.push(new HtmlWebpackPlugin({ template: './src/dev/index.html' }))
+    externals = []
+  }
+
+  return {
+    mode: 'development',
+    entry,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      library: {
+        type: 'umd',
+        name: 'ReactCompassNavigator',
       }
-    ]
-  },
-  externals: ["react"]
+    },
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.s?css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        }
+      ]
+    },
+    externals,
+    devServer: {
+      port: 3200
+    },
+  }
 }
